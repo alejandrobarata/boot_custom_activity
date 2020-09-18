4bb8c8a9b379b0b878b7e228287038fc08a922af
Alejandro Barata Alonso <abarata@clarcat.com>
2020-09-18 14:44:50 +0200

    Nueva prueba

--- src/main/resources/static/js/postmonger.js
+++ src/main/resources/static/js/postmonger.js
@@ -1,27 +1,4 @@
-'use strict';
-
-requirejs.config({
-  paths: {
-    postmonger: 'postmonger',
-  },
-  shim: {
-    'jquery.min': {
-      exports: '$',
-    },
-    '../customActivity': {
-      deps: ['jquery.min', 'postmonger'],
-    },
-  },
-});
-
-requirejs(['jquery.min', '../customActivity'], function ($, customEvent) {});
-
-requirejs.onError = function (err) {
-  if (err.requireType === 'timeout') {
-    console.log('modules: ' + err.requireModules);
-  }
-  throw err;
-}; //
+//
 /*
  * Postmonger.js   version 0.0.14
  * https://github.com/kevinparkerson/postmonger
